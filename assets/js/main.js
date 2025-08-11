3ee72459fde12f2664600dc3e06dda1c4c53b289
swavi89 <swaviman89@gmail.com>
2025-08-11 12:01:33 +0530

    Add font awesome icons

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// Typing Animation using Typed JS
+var typed = new Typed(".type", {
+    strings: ["a Web", "an Backend", "a FullStack"],
+    smartBackspace: true,
+    startDelay: 1000,
+    typeSpeed: 130,
+    backDelay: 1000,
+    backSpeed: 60,
+    loop: true,
+  });
